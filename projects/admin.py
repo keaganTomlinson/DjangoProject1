7eaa33abaf8441d1a1be50e4a469f3aa3287be0b
keaganTomlinson <keagantomlinson6@gmail.com>
2023-12-19 23:03:07 +0200

    final

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from projects.models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
