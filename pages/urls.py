47ef4a53ce22513ca687cc1285b2ebcd310a62f0
keaganTomlinson <keagantomlinson6@gmail.com>
2023-12-25 23:52:55 +0200

    updates

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,9 @@
 
 from django.urls import path
 from pages import views
+from projects import views
 
 urlpatterns = [
-    path("", views.home, name='home'),
+    path("", views.project_index, name="project_index"),  # Set project_index as the default homepage
+    path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from projects.models import Project
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        "projects": projects
+    }
+    return render(request, "projects/project_index.html", context)
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        "project": project
+    }
+    return render(request, "projects/project_detail.html", context)
+
 def home(request):
     return render(request, "pages/home.html", {})
